e8368c0aa86ac86645aae5b05e8dedf368ec0941
nm123 <16166118+nm123_7878@user.noreply.gitee.com>
2026-03-17 19:40:16 +0800

    init:新增pyproject.toml
    chore:增加.gitignore
    
    feat(auth):新增post接口

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# src/__init__.py
+
+
+
+class QzClient:
+    def __init__(self, token = None):
+        self.token = token
---
+++ src/core/post.py
@@ -0,0 +1,72 @@
+import requests
+import json
+class Post:
+    def __init__(self,token = ""):
+        self.base_url = "https://hehenya.dpdns.org:8503"
+        self.token = token        
+        self.headers = {
+            "Accept-Language": "zh-cn,zh;q=0.5",
+            "Accept-Charset": "UTF-8",
+            "x-access-token": self.token,
+            "Content-Type": "application/json",
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip"
+        }
+
+    def send_message(self, title, content, category_id, message_type=None, is_markdown=None):
+        if not config.token:
+            return "请先设置token"
+        
+        url = f"{self.base_url}/post_message"
+        data = {
+            "category_id": category_id,
+            "content": content
+        }
+        
+        if is_markdown:
+            data["is_markdown"] = is_markdown
+        if message_type:
+            data["message_type"] = message_type
+        if title:
+            data["title"] = title    
+        try:
+            response = requests.post(url, headers=self.headers, data=json.dumps(data))
+            return response.json()
+        except Exception as e:
+            return f"请求出错: {str(e)}"
+
+    def like_message(self, message_id):
+
+        url = f"{self.base_url}/like_message"
+        data = {"message_id": message_id}
+        
+        try:
+            response = requests.post(url, headers=self.headers, data=json.dumps(data))
+            return response.json()
+        except Exception as e:
+            return str(e)
+
+    def get_messages(self, category_id, page, per_page):
+        url = f"{self.base_url}/v3/get_message?category_id={category_id}&page={page}&per_page={per_page}"
+        
+        try:
+            response = requests.get(url, headers=self.headers)
+            return response.json()
+        except requests.RequestException as e:
+            return f"请求出错: {str(e)}"
+            
+    def reply_to_message(self, content, category_id, referenced_message_id):
+
+        url = f"{self.base_url}/post_referenced_message"
+        data = {
+            "content": content,
+            "category_id": category_id,
+            "referenced_message_id": referenced_message_id
+        }
+        
+        try:
+            response = requests.post(url, headers=self.headers, data=json.dumps(data))
+            return response.json()
+        except Exception as e:
+            return f"请求出错: {str(e)}"
+    
